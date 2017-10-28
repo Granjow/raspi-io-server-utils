c4bf8d8d25cd9df09b77010176f2d428d3d30af5
Simon A. Eugster <simon.eu@gmail.com>
2017-10-28 17:29:11 +0200

    Add vector clock class

---
+++ spec/vector-clock.spec.js
@@ -0,0 +1,80 @@
+const VectorClock = require( '../src/vector-clock/vector-clock' );
+
+describe( 'Vector clock', () => {
+    it( 'can be created', () => {
+        expect( () => new VectorClock( 'a' ) ).not.toThrow();
+    } );
+    it( 'throws an error for invalid ID', () => {
+        expect( () => new VectorClock( {} ) ).toThrow();
+    } );
+    it( 'increases its time on tick', () => {
+        const c = new VectorClock( 'a' );
+        expect( c.time ).toBe( 0 );
+
+        c.nextTick();
+        expect( c.time ).toBe( 1 );
+    } );
+} );
+
+describe( 'Synchronisation', () => {
+    it( 'uses newest timestamps', () => {
+        const a = new VectorClock( 'a' );
+        const b = new VectorClock( 'b' );
+        const c = new VectorClock( 'c' );
+
+        a.nextTick().nextTick().nextTick();
+        b.nextTick().nextTick();
+        c.nextTick();
+
+        a.syncFrom( b ).syncFrom( c );
+
+        expect( a.time ).toBe( 3 );
+        expect( a.timeOf( 'b' ) ).toBe( 2 );
+        expect( a.timeOf( 'c' ) ).toBe( 1 );
+    } );
+} );
+
+describe( 'Time comparison', () => {
+    it( 'recognizes newer timestamps', () => {
+        const c1 = new VectorClock( 'a' );
+        const c2 = new VectorClock( 'b' );
+
+        c1.syncFrom( c2 );
+        c2.syncFrom( c1 );
+
+
+        expect( c1.newerThan( c2 ) ).toBe( false, 'Equal timestamps should ' );
+        c1.nextTick();
+        expect( c1.newerThan( c2 ) ).toBe( true, 'C is newer after one tick' );
+    } );
+    it( 'detects when timestamps are not newer', () => {
+        const c1 = new VectorClock( 'a' );
+        const c2 = new VectorClock( 'b' );
+
+        c1.nextTick();
+
+        expect( c1.newerThan( c2 ) ).toBe( false, 'Not newer until synced' );
+    } );
+    it( 'considers unsynced clients', () => {
+        const a = new VectorClock( 'a' );
+        const b = new VectorClock( 'b' );
+        const c = new VectorClock( 'c' );
+
+        // c knows about a
+        c.syncFrom( a );
+        // a knows about b and c
+        a.syncFrom( b ).syncFrom( c );
+        // b knows about a and c
+        b.syncFrom( a );
+
+        // Event on c: not newer than a
+        c.nextTick();
+        expect( c.newerThan( a ) ).toBe( false, 'c does not know about b' );
+        expect( c.ownTimestampNewerThan( a ) ).toBe( true, 'Is newer when ignoring all other timestamps' );
+        expect( a.ownTimestampNewerThan( c ) ).toBe( false );
+
+        // Event on b: newer than a
+        b.nextTick();
+        expect( b.newerThan( a ) ).toBe( true, 'b knows about a and c and happened afterwards' );
+    } );
+} );
---
+++ src/vector-clock/vector-clock.js
@@ -0,0 +1,128 @@
+const check = require( 'check-types' );
+
+class VectorClock {
+
+    /**
+     * @param {string|{owner: string, time: Array}} data
+     */
+    constructor( data ) {
+
+        /** @type {Map.<string,number>} */
+        this._clock = new Map();
+
+        let ownId;
+        if ( check.string( data ) ) {
+            ownId = data;
+        } else {
+            if ( check.not.object( data ) ) throw new Error( 'Must pass either an ID (string) or a object to construct a vector clock' );
+            if ( check.not.string( data.owner ) ) throw new Error( 'owner is required when constructing from object' );
+            if ( check.not.array( data.time ) ) throw new Error( 'time vector is required when constructing from object' );
+
+            ownId = data.owner;
+            data.time.forEach( t => {
+                this.updateOther( t.id, t.time );
+            } );
+        }
+
+        /** @type {string} */
+        this._ownId = ownId;
+
+        if ( !this._clock.has( ownId ) ) {
+            this._clock.set( ownId, 0 );
+        }
+    }
+
+    get id() {
+        return this._ownId;
+    }
+
+    get time() {
+        return this._clock.get( this._ownId );
+    }
+
+    /**
+     * @returns {{id:string, time: number}[]}
+     */
+    get timestamps() {
+        const timestamps = [];
+        this._clock.forEach( ( v, k ) => timestamps.push( { id: k, time: v } ) );
+        return timestamps;
+    }
+
+    get json() {
+        return {
+            time: this.timestamps,
+            owner: this._ownId
+        };
+    }
+
+    nextTick() {
+        this._clock.set( this._ownId, this._clock.get( this._ownId ) + 1 );
+        return this;
+    }
+
+    /**
+     * @param {VectorClock} other
+     */
+    newerThan( other ) {
+        let newer = 0;
+        let newerOrEqual = 0;
+        this._clock.forEach( ( v, k ) => {
+            newer += v > other.timeOf( k );
+            newerOrEqual += v >= other.timeOf( k );
+        } );
+        const knowsAll = other.timestamps.every( timestamp => this._clock.has( timestamp.id ) );
+        return knowsAll && newerOrEqual > 0 && newer > 0;
+    }
+
+    /**
+     * Only checks the timestamp of this clock's owner, ignores all other timestamps
+     * @param {VectorClock} other
+     */
+    ownTimestampNewerThan( other ) {
+        return this.time > other.time;
+    }
+
+    /**
+     * Integrate vector time of other into own time (update timestamps of all clients)
+     * @param {VectorClock} other
+     */
+    syncFrom( other ) {
+        const errors = [];
+        other.timestamps.forEach( time => {
+            if ( time.id !== this._ownId ) {
+                this.updateOther( time.id, time.time );
+            } else {
+                if ( time.time > this.time ) {
+                    errors.push( 'Client thinks my time is newer' );
+                }
+            }
+        } );
+        if ( errors.length > 0 ) {
+            throw new Error( errors.join( ',' ) );
+        }
+        return this;
+    }
+
+    timeOf( id ) {
+        return this._clock.get( id );
+    }
+
+    toString() {
+        const times = [ `{Vector Clock for ${this._ownId}}` ];
+        this._vectorClock.forEach( ( v, k ) => times.push( `${v} : ${k}${k === this._ownId ? ' (self)' : ''}` ) );
+        return times.join( '\n' );
+    }
+
+    updateOther( id, time ) {
+        if ( id === this._ownId ) throw new Error( 'Other client has same ID: ' + id );
+        if ( check.not.string( id ) ) throw new Error( 'Client ID must be a string. Received: ' + id );
+        if ( check.not.greaterOrEqual( time, 0 ) ) throw new Error( `Time for client ${id} must be a number > 0` );
+
+        const currentTime = this._clock.get( id ) || 0;
+        this._clock.set( id, Math.max( currentTime, time ) );
+    }
+
+}
+
+module.exports = VectorClock;
