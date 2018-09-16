0096480de75a8b8ae7466fa1027561f63ec12d5c
Simon A. Eugster <simon.eugster@eps.ch>
2018-09-16 22:25:38 +0200

    Make DigitalOutput invertible

--- src/io/digital-output.ts
+++ src/io/digital-output.ts
@@ -7,16 +7,20 @@ const rpio = require( 'rpio' );
 export class DigitalOutput extends EventEmitter {
 
     private readonly _pin : number;
+    private readonly _low : number;
+    private readonly _high : number;
     private _enabled : boolean;
 
-    constructor( pin : number ) {
+    constructor( pin : number, inverted : boolean = false ) {
         super();
 
         this._pin = pin;
         this._enabled = false;
+        this._low = inverted ? rpio.HIGH : rpio.LOW;
+        this._high = inverted ? rpio.LOW : rpio.HIGH;
 
         try {
-            rpio.open( pin, rpio.OUTPUT, rpio.LOW );
+            rpio.open( pin, rpio.OUTPUT, this._low );
         } catch ( e ) {
             throw new Error( `Could not open input pin ${pin}: ${e.message || e}` );
         }
@@ -35,7 +39,7 @@ export class DigitalOutput extends EventEmitter {
 
     set enabled( enabled : boolean ) {
         this._enabled = enabled;
-        rpio.write( this._pin, enabled ? rpio.HIGH : rpio.LOW );
+        rpio.write( this._pin, enabled ? this._high : this._low );
     }
 
 }
